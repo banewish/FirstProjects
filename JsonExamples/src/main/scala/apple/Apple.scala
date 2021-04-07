b12cabe1f2d5aabb23c034ea21c5c89a7af17c04
Alexey Kiselev <alex.kiselev.dev@gmail.com>
2021-04-07 15:27:54 +0300

    init

--- JsonExamples/src/main/scala/apple/Apple.scala
+++ JsonExamples/src/main/scala/apple/Apple.scala
@@ -12,21 +12,22 @@ object AppleExample extends App {
 
     val greenApple = Apple("green", 0.35)
 
-
-  val json = greenApple.asJson.noSpaces
-  println(json)
-
-  val decodedApple = decode[Apple](json)
-  //println(decodedApple)
-
-  val jsonInStr = "{\n  \"color\": \"Red\",\n  \"weight\": 0.431\n}"
-
-  val decodedRedApple = decode[Apple](jsonInStr)
+//
+//  val json = greenApple.asJson.noSpaces
+// // println(json)
+//
+//  val decodedApple = decode[Apple](json)
+//  //println(decodedApple)
+//
+//  val jsonInStr = "{\n  \"color\": \"Red\",\n  \"weight\": 0.431\n}"
+//
+//  val decodedRedApple = decode[Apple](jsonInStr)
 
   //println(decodedRedApple)
 
   implicit val decoder: Decoder[Apple] = deriveDecoder[Apple] // достает из джисона в кейс класс
   implicit val encoder: Encoder[Apple] = deriveEncoder[Apple] // записываем в джисон
 
-
+val pizdec = Apple("grey", 22.8).asJson
+println(pizdec)
 }

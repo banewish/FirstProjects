7ae7270eab50c287bc872cc2e079ef06f4dd229d
Alexey Kiselev <alex.kiselev.dev@gmail.com>
2021-04-07 17:44:40 +0300

    init

--- JsonExamples/src/main/scala/apple/Apple.scala
+++ JsonExamples/src/main/scala/apple/Apple.scala
@@ -1,33 +1,53 @@
 package apple
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
 
 
+import io.circe.syntax._
   import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
   import io.circe.{Decoder, Encoder}
+
 object AppleExample extends App {
 
+  case class Apple(color: String, weight: Double, worm: Worm)
 
-  case class Apple(color: String, weight: Double)
+  case class Worm(name: String)
 
-
-    val greenApple = Apple("green", 0.35)
-
-//
-//  val json = greenApple.asJson.noSpaces
-// // println(json)
-//
-//  val decodedApple = decode[Apple](json)
-//  //println(decodedApple)
-//
-//  val jsonInStr = "{\n  \"color\": \"Red\",\n  \"weight\": 0.431\n}"
-//
-//  val decodedRedApple = decode[Apple](jsonInStr)
-
-  //println(decodedRedApple)
 
   implicit val decoder: Decoder[Apple] = deriveDecoder[Apple] // достает из джисона в кейс класс
   implicit val encoder: Encoder[Apple] = deriveEncoder[Apple] // записываем в джисон
+  implicit val decoderWorm: Decoder[Worm] = deriveDecoder[Worm] // достает из джисона в кейс класс
+  implicit val encoderWorm: Encoder[Worm] = deriveEncoder[Worm] // записываем в джисон
+  val worm = Worm("Jonh-Bastrad-Fucking-Ass")
+  val greenApple = Apple("green", 0.35, worm).asJson
+  println(greenApple)
 
-val pizdec = Apple("grey", 22.8).asJson
-println(pizdec)
+  println(greenApple.as[Apple])
+
+  println(greenApple.as[Apple].toOption.get)
+}
+object ExampleFirstAndSecond extends App{
+  case class D(a:String,b:Int)
+  case class C(a:String,b:Int,c:Double)
+  case class Number(a:Int,b:String,c:Double,d1: D,c1: C,seq:Seq[Int])
+
+
+  implicit val decoderD: Decoder[D] = deriveDecoder[D] // достает из джисона в кейс класс
+  implicit val encoderD: Encoder[D] = deriveEncoder[D] // записываем в джисон
+
+  implicit val decoderC: Decoder[C] = deriveDecoder[C] // достает из джисона в кейс класс
+  implicit val encoderC: Encoder[C] = deriveEncoder[C] // записываем в джисон
+
+  implicit val decoderNumber: Decoder[Number] = deriveDecoder[Number] // достает из джисона в кейс класс
+  implicit val encoderNumber: Encoder[Number] = deriveEncoder[Number] // записываем в джисон
+
+
+
+  val d = D("jopa svnutri",123)
+  val c = C("str",3,3.0)
+  val number = Number(1,"jopa",2.76,d,c,Seq(1,2,3)).asJson
+  println(number)
+  println(number.as[Number])
+
+
+
+
 }

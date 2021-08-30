0ab7a335b9ffd1c627d53938cac5ba252084ca35
Alexey Kiselev <alex.kiselev.dev@gmail.com>
2021-08-30 20:09:04 +0300

    init

--- examples/app/controllers/HomeController.scala
+++ examples/app/controllers/HomeController.scala
@@ -7,37 +7,23 @@ import play.api.data.Form
 import play.api.data.Forms._
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with play.api.i18n.I18nSupport{
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with play.api.i18n.I18nSupport {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    val passwordForm: Form[UserData] = Form(
-          mapping(
-            "password" -> number(min = 4, max = 15)
-          )(UserData.apply)(UserData.unapply)
-    )
-      passwordForm.bindFromRequest.fold(
-      formWithErrors => {
-        // binding failure, you retrieve the form containing errors:
-        BadRequest(views.html.index(formWithErrors))
-       },
-        UserData => {
-//          val newPassword = models.Password(UserData.password)
-          Redirect(routes.HomeController.index())
-        }
-      )
 
-    Ok(views.html.index(passwordForm))
+    Ok(views.html.index1())
   }
+// в роутс создать один динамический парт, для всех пейджей. получить из роут и отправить в index1 . MVC!!!
 
-  @Singleton
-  class Relative @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
-    def helloview() = Action { implicit request =>
-      Ok(views.html.hello()) // видимо нужно создать ещё что-то
-    }
-    def hello(password: Int) = Action {
-      Ok(s"Hi, your password is good")
-    }
+def showPage(FirstNumber:Int, SecondNumber:Int) = Action {
 
-  }
+  Ok("number of page is: " + (FirstNumber+SecondNumber))
 }
-case class UserData(password: Int)
+
+}
+
+
+
+
+
+
